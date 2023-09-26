f60e782d73d95dc103981a51cb38fb0c2b21a1e2
Ashish kumar <ashish.k@bindapi.com>
2023-09-26 13:59:27 +0530

    first commit

---
+++ video-1/index.php
@@ -0,0 +1,20 @@
+<?php
+abstract class parentClass
+{
+    public $name;
+
+    abstract protected function calc($a, $b);
+}
+
+class childClass extends parentClass
+{
+
+    public function calc($c, $d)
+    {
+        echo $c + $d;
+    }
+}
+
+$test = new childClass();
+
+$test->calc(23, 23);
