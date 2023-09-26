12944a8faa08caa0c4c86c681c729ae7adaf2553
Ashish kumar <ashish.k@bindapi.com>
2023-09-26 14:35:58 +0530

    init

--- video-2/access_modifires.php
+++ video-2/access_modifires.php
@@ -1,2 +1,27 @@
 <?php
-    
+class Base
+{
+    protected $name;
+
+    public function __construct($n)
+    {
+        $this->name = $n;
+    }
+
+    public function show()
+    {
+        echo "Your Name : " . $this->name . "<br>";
+    }
+}
+
+class Derived extends Base
+{
+    public function get()
+    {
+        echo "Your Name : " . $this->name . "<br>";
+    }
+}
+
+$test = new Derived("Ashish Kumar");
+// $test->name = "bsbdsjkdkjas";
+$test->get();
