f5b4d60f5ecf56ea33b874e4d1a14b0386e3abce
ashish kumar <ashish.k@bindapi.com>
2023-10-23 11:04:43 +0530

    init

--- video-6/inheritance.php
+++ video-6/inheritance.php
@@ -15,27 +15,28 @@ class Employee
     function info()
     {
         echo "<h3>Employee  Profile</h3>";
-        echo "Employee nmae : ".$this->name."<br>";
-        echo "Employee age : ".$this->age."<br>";
-        echo "Employee salary : ".$this->salary."<br>";
+        echo "Employee nmae : " . $this->name . "<br>";
+        echo "Employee age : " . $this->age . "<br>";
+        echo "Employee salary : " . $this->salary . "<br>";
     }
 }
-class Manager extends Employee {
+class Manager extends Employee
+{
     public $ta = 1000;
     public $phone = 300;
     public $totalsalary;
 
     function info()
     {
-        $this->totalsalary =$this->salary + $this->ta + $this->phone;
+        $this->totalsalary = $this->salary + $this->ta + $this->phone;
         echo "<h3>Manager  Profile</h3>";
-        echo "Manager nmae : ".$this->name."<br>";
-        echo "Manager age : ".$this->age."<br>";
-        echo "Manager salary : ".$this->totalsalary."<br>";
-    } 
+        echo "Manager nmae : " . $this->name . "<br>";
+        echo "Manager age : " . $this->age . "<br>";
+        echo "Manager salary : " . $this->totalsalary . "<br>";
+    }
 }
 
-$test = new Employee('Anand Kumar',20,10000);
+$test = new Employee('Anand Kumar', 20, 10000);
 $test->info();
-$test2 = new Manager('Sarvrsh Kumar',30,22000);
+$test2 = new Manager('Sarvrsh Kumar', 30, 22000);
 $test2->info();
